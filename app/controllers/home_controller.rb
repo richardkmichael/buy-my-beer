4e7097eb8372b05a6ebcd0c50b144a812d543628
Richard Michael <rmichael@edgeofthenet.org>
2011-07-21 20:25:20 +0200

    Remove class variables in views, flash errors on failure, list users on the index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < PagesController
     @message = 'Home#index'
     @current_user = current_user
 
+    # TODO: Q: index.haml.html -> _project.haml.html which queries for users.  Eager load?
     @projects = current_user.projects
   end
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,24 +1,19 @@
 class ProjectsController < ApplicationController
+
   def new
     @project = Project.new
-    @current_user = current_user
   end
 
   def create
     @project = Project.new(params[:project])
-    # @project.users << current_user
+    @project.users << current_user
 
     if @project.save
-      flash[:success] = 'Project successfully created!'
-      # TODO: Q: What's the best way to do this?
-      redirect_to :controller => 'home', :action => 'index'
+      flash[:success] = 'Create project succeeded.'
+      redirect_to root_path
     else
-      @project.name = nil
-
-      # TODO: Q: 'new' requires @current_user, why isn't it defined?
-      # TODO: Q: render() doesn't include layouts (so we miss flash messages), etc.?  Why?
-      @current_user = current_user
-      render :new
+      flash[:failure] = "Create project failed: #{@project.errors.full_messages}."
+      render :new, :layout => true
     end
 
   end
