76a5873134bc9623b7c6c998c43508ebae451346
Richard Michael <rmichael@edgeofthenet.org>
2011-07-21 22:09:14 +0200

    Delete link in the index, destroy action.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,6 +15,20 @@ class ProjectsController < ApplicationController
       flash[:failure] = "Create project failed: #{@project.errors.full_messages}."
       render :new, :layout => true
     end
-
   end
+
+  def destroy
+    @project = Project.find(params[:project])
+
+    if @project.destroy
+      flash[:success] = 'Delete project succeeded.'
+    else
+      flash[:failure] = "Delete project failed: #{@project.errors.full_messages}."
+    end
+
+    # There is no destroy.html.haml, always redirect to the index.
+    # TODO: Q: Use render() instead?  Why round-trip to the browser?
+    redirect_to root_path
+  end
+
 end
