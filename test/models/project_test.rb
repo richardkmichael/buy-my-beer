d9290b75e2d70cd554d147172031b17ce0ac4280
Richard Michael <rmichael@edgeofthenet.org>
2011-07-25 13:48:07 +0200

    Write tests to insure projects manage their associated models correctly.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,3 +1,6 @@
+# TODO: Q: What about "after do ; @project.save ; end' to be sure the record always saves?
+#          This would slow down the testsuite.
+
 require "minitest_helper"
 
 # TODO: Q: Why ::Rails::Model, instead of ::AR::Model?  Fixture glue is a railtie?
@@ -5,12 +8,11 @@ require "minitest_helper"
 class ProjectTest < MiniTest::Rails::Model
 
   before do
-    # TODO: Use a Factory here?
+    # TODO: Use Factories here?
+    @user =  User.new(:email => 'user@example.com', :password => 'password')
     @project = Project.new(:name => 'Test Project')
-    @project.users << User.new(:email => 'user@example.com')
+    @project.users << @user
   end
-
-  # TODO: Q: How are these three tests different than just calling .valid? on @project above?
 
   it 'must have a name' do
     refute_empty @project.name, 'The project has no name.'
@@ -25,9 +27,51 @@ class ProjectTest < MiniTest::Rails::Model
     refute_empty @project.url, 'The project has no URL.'
   end
 
+  # Really?
+  # Should we implicitly make the project-creating-user a collaborator?
+  # What about a user creating a project who is not a collaborator on THAT same project?
   it 'must have a user' do
+    @project.users << @user
     refute_empty @project.users, 'The project has no users.'
   end
+
+  # TODO: The following tests really need before/after setup/teardown to insure
+  # TODO: objects are in the DB but are also properly removed at the end of the
+  # TODO: testcase (to avoid leakage to other tests when randomized.
+  # -----------------------------------------
+
+  # TODO: Q: This feels wrong.  The testsuite is now dependent on the DB state.
+  # TODO: Q: What if another test creates a Collaboration? :/
+  it 'should delete collaborations when deleted' do
+
+    # We must persist @project so that ActiveRecord will create the collaboration record.
+    @user.save
+    @project.save
+
+    assert_equal(1, Collaboration.all.count, 'Project collaborators were not created.')
+
+    @project.destroy
+    assert_equal(0, Collaboration.all.count, 'Project collaborators were not destroyed.')
+
+    @user.destroy
+  end
+
+  it 'should not delete users when deleted' do
+
+    # Put a user and project in the DB.
+    @user.save
+    @project.save
+
+    assert_equal(@user, User.find_by_email(@user.email))
+
+    @project.destroy
+    assert_equal(@user, User.find_by_email(@user.email))
+
+    @user.destroy
+  end
+
+  # -----------------------------------------
+
 
   # it 'must have a unique url' do
   #   # TODO: A uniqueness test on the URL.
