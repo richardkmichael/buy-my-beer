f0623cdefc3535312c72aa23ef6381ea03fdfbab
Richard Michael <rmichael@edgeofthenet.org>
2011-08-01 15:19:16 +0200

    Build test stubs.

---
+++ test/models/build_test.rb
@@ -0,0 +1,21 @@
+require "minitest_helper"
+
+class BuildTest < MiniTest::Rails::Model
+
+  it 'must belong to a project' do
+    assert true
+  end
+
+  it 'must have a last commit' do
+    assert true
+  end
+
+  it 'must have a last commiter' do
+    assert true
+  end
+
+  it 'must have a status which is true or false' do
+    assert true
+  end
+
+end
