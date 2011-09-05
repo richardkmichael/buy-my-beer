d6b25cfe13fbed5921eb6df45f5f821f9b2e7c33
Richard Michael <rmichael@edgeofthenet.org>
2011-09-05 20:44:40 +0200

    Comment out the custom mime type for now to silence warnings.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file, even in development mode.
 
-# TODO: Why can't I register more than 1 mime type against :json?  See: "rails c" warning.
-Mime::Type.register 'application/vnd.build-v1+json', :json
+# Mime::Type.register 'application/vnd.build-v1+json', :json
