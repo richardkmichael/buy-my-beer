d41c77f4df614af7abafd9979e0803b3d1cd28ae
Richard Michael <rmichael@edgeofthenet.org>
2011-09-09 11:01:23 +0200

    Start the controller tests.

--- test/controllers/project_controller_test.rb
+++ test/controllers/project_controller_test.rb
@@ -1,11 +1,13 @@
 require "minitest_helper"
 
 class ProjectsControllerTest < MiniTest::Rails::Controller
-  # before do
-  #   @controller = ProjectsController.new
-  # end
 
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    @controller = ProjectsController.new
+  end
+
+  it 'must respond to get' do
+    response = get :index
+    puts response
+  end
 end
