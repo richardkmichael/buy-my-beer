831205240c5a69082a5b2c690c2bd56737becfd1
Richard Michael <rmichael@edgeofthenet.org>
2011-10-22 22:02:00 +0200

    Begin test writing.

--- test/functional/builds_controller_test.rb
+++ test/functional/builds_controller_test.rb
@@ -1,11 +1,31 @@
 require "test_helper"
 
 class BuildsControllerTest < ActionController::TestCase
-  # before do
-  #   @controller = BuildsController.new
+
+  # def setup
+  #   DatabaseCleaner.start
   # end
 
-  # it 'must create a new user if the last commiter is not already a user' do
-  #   assert false
+  # def teardown
+  #   DatabaseCleaner.clean
   # end
+
+  test 'it must create a build when sent JSON' do
+    @build = Factory.build :build
+
+    puts @build
+
+    assert_equal @build.class, Build
+
+    # post :create, :format => :json, :build => @build.to_json, :uuid => @build.project.uuid
+    post :create, :format => :json, :build => @build, :uuid => @build.project.uuid
+
+    # puts @response
+    # puts @response.body
+
+    assert_response :success
+    # assert_difference 'Build.count' do
+    # end
+  end
+
 end
