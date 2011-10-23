7c2dc9a154e545590131abe2575bd020a6c2c745
Richard Michael <rmichael@edgeofthenet.org>
2011-10-23 12:23:17 +0200

    BuildController tests.

--- test/functional/builds_controller_test.rb
+++ test/functional/builds_controller_test.rb
@@ -2,30 +2,48 @@ require "test_helper"
 
 class BuildsControllerTest < ActionController::TestCase
 
-  # def setup
-  #   DatabaseCleaner.start
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+
+  # test 'it must only allow creation by POST of JSON' do
+  #   # This is a router test.
   # end
 
-  # def teardown
-  #   DatabaseCleaner.clean
-  # end
+  test 'it must respond with success after a create build' do
 
-  test 'it must create a build when sent JSON' do
-    @build = Factory.build :build
+    # Use a factory to prepare the database with everything for the POSTed build to validate.
+    build = Factory.build :build
+    assert_equal build.class, Build
 
-    puts @build
-
-    assert_equal @build.class, Build
-
-    # post :create, :format => :json, :build => @build.to_json, :uuid => @build.project.uuid
-    post :create, :format => :json, :build => @build, :uuid => @build.project.uuid
-
-    # puts @response
-    # puts @response.body
+    assert_difference 'Build.count' do
+      post :create, :format        => :json,
+                    :last_commiter => build.last_commiter.email,
+                    :uuid          => build.project.uuid,
+                    :build         => { :status => build.status,
+                                        :last_commit => build.last_commit },
+    end
 
     assert_response :success
-    # assert_difference 'Build.count' do
-    # end
+  end
+
+  # This should really be two tests.
+  test 'it must redirect to the project page after a delete ' do
+    build = Factory.create :build
+
+    assert_difference 'Build.count', -1 do
+      post :destroy, :id => build.id
+    end
+    assert_redirected_to project_path(build.project)
+  end
+
+  test 'it must redirect to the index page after a failed delete' do
+    post :destroy, :id => '100' # build.id = 100 does not exist.
+    assert_redirected_to root_path
   end
 
 end
