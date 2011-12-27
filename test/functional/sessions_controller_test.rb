00ea609f2ec399140b6806396c64ed0d62e36fb5
Richard Michael <rmichael@edgeofthenet.org>
2011-12-27 16:31:04 -0500

    Start the SessionController work.

---
+++ test/functional/sessions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+
+  # test 'it must sign in' do
+  # end
+  #
+  # test 'it must sign out' do
+  # end
+  #
+  # test 'it must create a new account if one does not exist' do
+  # end
+  #
+  # test 'it must sign in after creating an account' do
+  #   # THIS MUST TEST THAT ACTIONS CAN BE PERFORMED AFTER SIGN IN
+  #   #  EX. The session is initialized correctly for subsequent requests.
+  # end
+
+end
