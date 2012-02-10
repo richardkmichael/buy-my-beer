a17df38f2480d273a65a860e3ba52bb110ae5cb7
Richard Michael <rmichael@edgeofthenet.org>
2012-02-09 20:40:51 -0500

    Remove malformed JSON testing, Rails does this (but investigate); add JSON POST tests.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,30 +1,24 @@
 class BuildsController < ApplicationController
 
   def create
-    begin
-      build = Build.new( :status      => params[:build][:status],
-                         :last_commit => params[:build][:last_commit] )
+    build = Build.new( :status      => params[:build][:status],
+                       :last_commit => params[:build][:last_commit] )
 
-      # Find the owning project.
-      build.project = Project.find_by_uuid(params[:build][:uuid])
+    # Find the owning project.
+    build.project = Project.find_by_uuid(params[:build][:uuid])
 
-      # Find the last commiter.
-      build.last_commiter = User.find_by_email(params[:build][:last_commiter])
+    # Find the last commiter.
+    build.last_commiter = User.find_by_email(params[:build][:last_commiter])
 
-      # Save the build, respond with it as JSON.
-      if build.valid?
-        build.save
-        render :json   => build,
-               :status => 200
-      else
-        render :json   => { :message => 'Invalid build.',
-                            :errors  => build.errors.full_messages,
-                            :build   => build },
-               :status => 400
-      end
-
-    rescue MultiJson::DecodeError => e
-      render :json   => { :message => 'Received malformed JSON.' },
+    # Save the build, respond with it as JSON.
+    if build.valid?
+      build.save
+      render :json   => build,
+             :status => 200
+    else
+      render :json   => { :message => 'Invalid build.',
+                          :errors  => build.errors.full_messages,
+                          :build   => build },
              :status => 400
     end
   end
--- test/functional/builds_controller_test.rb
+++ test/functional/builds_controller_test.rb
@@ -37,16 +37,62 @@ class BuildsControllerTest < ActionController::TestCase
 
   end
 
+  test 'it must create a build when JSON POSTed valid attributes' do
+
+    # Use a factory to prepare related models; ensure the factory and validations agree.
+    build = Factory.build :build
+    assert build.valid?
+
+    post :create, :format => :json, :build => { :uuid          => build.project.uuid,
+                                                :status        => build.status,
+                                                :last_commit   => build.last_commit,
+                                                :last_commiter => build.last_commiter.email }
+
+    assert_response :success
+
+    assert_block('Expected to response to be JSON.') { @response_build = JSON.parse @response.body }
+    assert_kind_of Hash, @response_build
+
+    assert_equal( @response_build['status'],           build.status )
+    assert_equal( @response_build['last_commit'],      build.last_commit )
+    assert_equal( @response_build['last_commiter_id'], build.last_commiter.id )
+
+    assert_equal 1, Build.count, "There should be 1 build, but there are #{Build.count}"
+
+  end
+
   test 'it must not create a build when POSTed invalid attributes' do
 
     # Use a factory to insure related models are prepared for the build.
     build = Factory.build :build
     assert build.valid?
 
-    # Post without the required UUID.
+    # Post without the required UUID attribute.
     post :create, :build => { :status        => build.status,
                               :last_commit   => build.last_commit,
                               :last_commiter => build.last_commiter.email }
+
+    assert_response :bad_request
+
+    assert_block('Expected to response to be JSON.') { @response_json = JSON.parse @response.body }
+    assert_kind_of Hash, @response_json
+
+    assert_match @response_json['message'], /^Invalid build/
+
+    assert_equal 0, Build.count, "There should be 0 build(s), but there are #{Build.count}"
+
+  end
+
+  test 'it must not create a build when JSON POSTed invalid attributes' do
+
+    # Use a factory to insure related models are prepared for the build.
+    build = Factory.build :build
+    assert build.valid?
+
+    # Post without the required UUID attribute.
+    post :create, :format => :json, :build => { :status        => build.status,
+                                                :last_commit   => build.last_commit,
+                                                :last_commiter => build.last_commiter.email }
 
     assert_response :bad_request
 
